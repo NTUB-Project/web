d1b849b5e91319c964fe3ac38b788541f9e44cd6
JiaNing <mo359612@gmail.com>
2018-05-15 16:35:28 +0800

    gem devise & install

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootstrap', '~> 4.0'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'octicons'
 gem 'octicons_helper'
+gem 'devise', '~> 4.4', '>= 4.4.3'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   resources :equipments, :foods, :grounds, :rentcars, :costumes, :photography, :custommade
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
