27dfb6f2b29d0ae606d6e1fa97750cff27a975ba
JiaNing <mo359612@gmail.com>
2018-05-18 00:03:55 +0800

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_05_15_122944) do
-
 ActiveRecord::Schema.define(version: 2018_05_15_135738) do
-
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
