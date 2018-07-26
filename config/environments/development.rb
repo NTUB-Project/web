a642d3314743e5df7f9d42af9777af0436392b7f
JiaNing <mo359612@gmail.com>
2018-07-26 11:17:07 +0800

    update email information

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://frozen-mesa-16727.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://frozen-mesa-16727.herokuapp.com/" }
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
 
