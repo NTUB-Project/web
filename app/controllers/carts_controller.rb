4eb618528a63b08edf961e55f8b7dacd548b79ea
JiaNing <mo359612@gmail.com>
2018-07-31 23:41:49 +0800

    no message

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,29 +2,16 @@ class CartsController < ApplicationController
 
     def add
       current_cart.add_item(params[:id])
-<<<<<<< HEAD
-      session[User::SessionKey] = current_cart.serialize
-=======
-      session[current_user.id] = current_cart.serialize
->>>>>>> bb0dd4bb681ef3475f0364927b9411f931f81c2e
+      session[:cart9487] = current_cart.serialize
     end
 
     def remove
       current_cart.delete_item(params[:id])
-<<<<<<< HEAD
-      session[User::SessionKey] = current_cart.serialize
-      redirect_to "/cart"
-    end
-
-    def destroy
-      session[User::SessionKey] = nil
-      redirect_to cart_path, notice: "蒐藏已全部清空"
-    end
-=======
-      session[current_user.id] = current_cart.serialize
+      session[:cart9487] = current_cart.serialize
       redirect_to "/cart"
     end
 
 
->>>>>>> bb0dd4bb681ef3475f0364927b9411f931f81c2e
+
+
 end
--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,11 +1,7 @@
 module CartsHelper
-<<<<<<< HEAD
-  def current_cart
-    @cart ||= Cart.from_hash(session[User::SessionKey])
-  end
-=======
+
     def current_cart
-      @cart ||= Cart.from_hash(session[current_user.id])
+      @cart ||= Cart.from_hash(session[:cart9487])
     end
->>>>>>> bb0dd4bb681ef3475f0364927b9411f931f81c2e
+
 end
