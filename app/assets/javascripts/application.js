7375460cf697359288116eb5390d163acf698e97
JiaNing <mo359612@gmail.com>
2018-09-28 00:35:39 +0800

    關於我們頁面修正、關閉turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,15 +11,13 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
-//= require_tree .
 
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
-
+//= require bootstrap-sprockets
+//= require_tree .
 
 
 var bkgrdimg_letters = {};
