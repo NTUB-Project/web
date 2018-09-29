93eaef0a0cef7beaaf4d42491d4298279fe3e340
SHUYU <10446014@ntub.edu.tw>
2018-09-29 12:49:55 +0000

    寄信-自行輸入信件內容(未完成)

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,9 @@ Rails.application.routes.draw do
     collection do
       post :add, path:'add/:id'
       put :remove, path:'/:id'
-      put :email, path:'email/:id'
+      # post :email, path:'email/:id'
+      put :matter, path:'matter/:id'
+      post :matter,path:'matter/:id'
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_062152) do
+ActiveRecord::Schema.define(version: 2018_09_29_032526) do
 
   create_table "activity_kinds", force: :cascade do |t|
     t.string "title"
@@ -44,45 +44,8 @@ ActiveRecord::Schema.define(version: 2018_09_12_062152) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "commontator_comments", force: :cascade do |t|
-    t.string "creator_type"
-    t.integer "creator_id"
-    t.string "editor_type"
-    t.integer "editor_id"
-    t.integer "thread_id", null: false
-    t.text "body", null: false
-    t.datetime "deleted_at"
-    t.integer "cached_votes_up", default: 0
-    t.integer "cached_votes_down", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cached_votes_down"], name: "index_commontator_comments_on_cached_votes_down"
-    t.index ["cached_votes_up"], name: "index_commontator_comments_on_cached_votes_up"
-    t.index ["creator_id", "creator_type", "thread_id"], name: "index_commontator_comments_on_c_id_and_c_type_and_t_id"
-    t.index ["thread_id", "created_at"], name: "index_commontator_comments_on_thread_id_and_created_at"
-  end
-
-  create_table "commontator_subscriptions", force: :cascade do |t|
-    t.string "subscriber_type", null: false
-    t.integer "subscriber_id", null: false
-    t.integer "thread_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["subscriber_id", "subscriber_type", "thread_id"], name: "index_commontator_subscriptions_on_s_id_and_s_type_and_t_id", unique: true
-    t.index ["thread_id"], name: "index_commontator_subscriptions_on_thread_id"
-  end
-
-  create_table "commontator_threads", force: :cascade do |t|
-    t.string "commontable_type"
-    t.integer "commontable_id"
-    t.datetime "closed_at"
-    t.string "closer_type"
-    t.integer "closer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["commontable_id", "commontable_type"], name: "index_commontator_threads_on_c_id_and_c_type", unique: true
+    t.integer "rating"
+    t.json "images"
   end
 
   create_table "identities", force: :cascade do |t|
@@ -92,6 +55,12 @@ ActiveRecord::Schema.define(version: 2018_09_12_062152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_identities_on_user_id"
+  end
+
+  create_table "matters", force: :cascade do |t|
+    t.text "mattertext"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "people_numbers", force: :cascade do |t|
