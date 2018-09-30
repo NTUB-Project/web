3daf64f042694fbf0fb8fc62e48b4e9450d9275b
JiaNing <mo359612@gmail.com>
2018-09-30 11:57:11 +0800

    後台：修正Product 的 index.html.erb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :activity_kind
   belongs_to :people_number
+  belongs_to :region
+  belongs_to :activity_kind
   has_many :comments
   mount_uploaders :images, ImageUploader
   has_and_belongs_to_many :regions
