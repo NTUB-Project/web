e37dea8c474fcce304df1d4bbf09073756e7feca
SHUYU <10446014@ntub.edu.tw>
2018-10-10 09:24:40 +0000

    no change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_173046) do
+ActiveRecord::Schema.define(version: 2018_10_06_170910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2018_09_29_173046) do
     t.datetime "updated_at", null: false
     t.integer "rating"
     t.json "images"
+  end
+
+  create_table "gmaps", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "identities", force: :cascade do |t|
