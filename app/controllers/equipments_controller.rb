57d25b99ca70d9bf40750a35f3116737ebd24e1d
JiaNing <mo359612@gmail.com>
2018-10-14 09:54:52 +0800

    除廠商外篩選完成

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -15,16 +15,27 @@ class EquipmentsController < ApplicationController
 
   def search
     all = Product.where(category: Category.find_by(title: "設備").id)
-    find = all.where(region: params[:region_ids]).or(all.where(activity_kind_id: params[:activity_kind_ids]))
-    equipment = find.group("name").select("MIN(id) AS id , name")
-    if equipment != []
+    find = all.group("name").select("MIN(id) AS id , name")
+
+    region = find.where(region: params[:region_ids]).to_a
+    region_name = []
+    region.map {|i| region_name << i.name}
+
+    activity_kind = find.where(activity_kind_id: params[:activity_kind_ids]).to_a
+    activity_kind_name = []
+    activity_kind.map {|i| activity_kind_name << i.name}
+
+    product = [region_name,activity_kind_name].reject(&:empty?).reduce(:&) || []
+
+    if product != []
       @equipments = []
-      0.upto(equipment.to_a.count-1) do |i|
-        @equipments <<  Product.find_by(id: equipment[i].id)
+      0.upto(product.count-1) do |i|
+        @equipments <<  Product.find_by(name: product[i])
       end
     else
       redirect_to equipments_path, notice: "無搜尋到此條件"
     end
+    
     #checkbox
     @regions = Region.all
     @activity_kinds = PeopleNumber.all
