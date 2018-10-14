4e343b1914136f7e79210129b101e073d321ed7f
JiaNing <mo359612@gmail.com>
2018-10-14 23:46:06 +0800

    新增蒐藏庫匯出廠商功能（未完成）

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,16 @@
 class Cart < ApplicationRecord
-has_many :cart_items
-has_many :products, through: :cart_items, source: :product
+  require 'csv'
+  has_many :cart_items
+  has_many :products, through: :cart_items, source: :product
 
+  def self.to_csv(options={})
+    attributes = %w{ id name email  }
+    CSV.generate(options) do |csv|
+      csv << attributes
+      all.each do |product|
+        csv << product.attributes.values_at(*attributes)
+      end
+    end.encode('gb2312', :invalid => :replace, :undef => :replace, :replace => "?")
+  end
 
 end
