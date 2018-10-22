2a8b69426af807c436d7b6970efe7523714e22c8
SHUYU <10446014@ntub.edu.tw>
2018-10-22 13:41:25 +0000

    master

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2018_10_16_221553) do
   end
 
   create_table "identities", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "provider"
     t.string "uid"
     t.datetime "created_at", null: false
@@ -121,10 +121,10 @@ ActiveRecord::Schema.define(version: 2018_10_16_221553) do
     t.string "email"
     t.string "url"
     t.text "equipment"
-    t.integer "category_id"
-    t.integer "region_id"
-    t.integer "activity_kind_id"
-    t.integer "people_number_id"
+    t.bigint "category_id"
+    t.bigint "region_id"
+    t.bigint "activity_kind_id"
+    t.bigint "people_number_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "images"
@@ -168,4 +168,5 @@ ActiveRecord::Schema.define(version: 2018_10_16_221553) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "identities", "users"
 end
