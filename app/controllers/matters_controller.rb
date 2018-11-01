8e7b55b72518befa7382766ad9c21d84fa73baa6
JiaNing <mo359612@gmail.com>
2018-11-02 00:39:19 +0800

    寄件夾增加頁數

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -1,7 +1,7 @@
 class MattersController < ApplicationController
   def index
-    @matter = current_user.matters
-    @matter_form = current_user.matter_forms
+    @matter_form = current_user.matter_forms.order('created_at desc' ).paginate(page: params[:page], per_page: 10)
+    @matter = current_user.matters.order('created_at desc' ).paginate(page: params[:page], per_page: 10)
   end
 
 end
