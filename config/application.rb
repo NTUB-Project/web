e1250b5abd3e8eea9b10597c7954c92f9c9b98c5
SHUYU <10446014@ntub.edu.tw>
2018-11-01 07:14:39 +0000

    行動版介面修改

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,11 @@ Bundler.require(*Rails.groups)
 
 module Web
   class Application < Rails::Application
+    config.time_zone = "Taipei"
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
